a3b08d9459ca12d6a44f2fdb7105b127e099d6bc
JettChenT <jettchen12345@gmail.com>
2025-08-02 16:11:09 +0800

    feat: tailwind and shadcn

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import reactLogo from "@/assets/react.svg";
 import wxtLogo from "/wxt.svg";
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="w-[300px] h-[500px]">
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <Button>
+        This is a button, click me!
+      </Button>
+    </div>
   );
 }
 
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,20 +1,30 @@
 import { defineConfig } from "wxt";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-react"],
-  webExt: {
-    binaries: {
-      helium: "/Applications/Helium.app/Contents/MacOS/Helium",
-    },
-    chromiumArgs: ["--user-data-dir=./.wxt/browser-data"],
-  },
-  manifest: {
-    web_accessible_resources: [
-      {
-        resources: ["x-mainworld.js"],
-        matches: ["*://*/*"],
-      },
-    ],
-  },
+	modules: ["@wxt-dev/module-react"],
+	webExt: {
+		binaries: {
+			helium: "/Applications/Helium.app/Contents/MacOS/Helium",
+		},
+		chromiumArgs: ["--user-data-dir=./.wxt/browser-data"],
+	},
+	manifest: {
+		web_accessible_resources: [
+			{
+				resources: ["x-mainworld.js"],
+				matches: ["*://*/*"],
+			},
+		],
+	},
+	vite: () => ({
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./"),
+			},
+		},
+	}),
 });
